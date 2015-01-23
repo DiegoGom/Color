6b736ab5aa1f6c6812052f7c2025eb84c1d06b92
Omar Ramírez <rigter.om@gmail.com>
2015-01-23 12:53:00 -0600

    Fixes
    - Cambios en el código
    - Organización de los archivos
    - Cambios en las versiones de las dependencias en package.json

--- index.js
+++ index.js
@@ -1,22 +1,13 @@
-/**
- * Server.js
- * @author : DiganmeGiovanni | https://twitter.com/DiganmeGiovanni
- * @Created on: 25 Oct, 2014
- */
- 
-/* Librerias necesarias para la aplicación */
-var app  = require('express')();
-var http = require('http').Server(app);
-var io   = require('socket.io')(http);
+var app = require('express')();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
 var _ = require('underscore');
- 
- var participantes=[];
- 
-/** *** *** ***
- *  Configuramos el sistema de ruteo para las peticiones web
- *  de manera que sin importar la ruta que el usuario solicite
- *  siempre lo direccionaremos al html del sistema de chat.
- */
+var participantes = [];
+
+server.listen(3000,function(){
+  console.log('+++ Servidor Iniciado +++');
+});
+
 app.get('/', function(req, res) {
   res.sendFile( __dirname + '/index.html');
 });
@@ -25,39 +16,21 @@ app.get('/cambiar', function(req, res) {
   res.sendFile( __dirname + '/cambiar.html');
 });
  
-/** *** *** ***
- *  Configuramos Socket.IO para estar a la escucha de
- *  nuevas conexiones.
- */
 io.on('connection', function(socket) {
-
-  console.log('Envio de peticion');
   
-  
-  socket.on('chat message', function(data) {
-    io.emit('chat message', {msg:data.msg,id:socket.id});
+  socket.on('envio', function(data) {
+    io.emit('envio', {msg:data.msg,id:socket.id});
+    console.log('- El usuario con ID: '+socket.id+' envio el color: '+data.msg);
   });
 
-  socket.on('newUser', function(data) {
+  socket.on('conexion', function(data) {
     participantes.push({id:data.id});
-    
+    console.log('- Usuario conectado con ID: '+socket.id);
   });
   
-  /**
-   * Mostramos en consola cada vez que un usuario
-   * se desconecte del sistema.
-   */
   socket.on('disconnect', function() {
     participantes=_.without(participantes,_.findWhere(participantes,{id:socket.id}));
-    console.log('Usuario desconectado');
+    console.log('- Desconexion del usuario con ID: '+socket.id);
   });
   
-});
- 
- 
-/**
- * Iniciamos la aplicación en el puerto 3000
- */
-http.listen(3000, function() {
-  console.log('listening on *:3000');
 });
